fe593fe60b78e4db4f21ec79cef7f8f03b7113ca
harleynghx <harleynghx.edu@gmail.com>
2024-04-09 01:11:22 +0800

    apr9

---
+++ pipexHeader.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipexHeader.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hang <hang@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/09 00:59:05 by hang              #+#    #+#             */
+/*   Updated: 2024/04/09 01:06:54 by hang             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+
+#endif
